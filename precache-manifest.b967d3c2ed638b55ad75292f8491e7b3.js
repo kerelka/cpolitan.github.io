ed7198aec27b4d7c7396dab6bdb2f6336018212a
kerelka <kerelkaa@gmail.com>
2020-07-03 22:43:37 +0700

    commit m -'deploy"
    deploy pertama

---
+++ precache-manifest.b967d3c2ed638b55ad75292f8491e7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac0ab1d72cc42ca3759b429628b07ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "569544dd22f7ded9bdca",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7dfcd36bd7ebc61c99e4",
+    "url": "/static/js/2.6cb3f8b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6cb3f8b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569544dd22f7ded9bdca",
+    "url": "/static/js/main.ba2a92c5.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
